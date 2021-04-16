abea4f462a26fa52ac9f7d604f597bee4cf45ff2
vadimszzz <vadimszzz@mail.ru>
2021-04-16 18:38:30 +0000

    Task 4 complete

--- src/Task4.cpp
+++ src/Task4.cpp
@@ -3,3 +3,15 @@
  * maximum and minimum 32-bit unsigned integer numbers which can be obtained
  * using the same number of bits set to 1 as in the taken number.
  */
+
+#include <inttypes.h>
+#include <tuple>
+
+
+std::tuple <uint32_t, uint32_t> squeezeOutEverythingOfThisBits(uint32_t n) {
+    uint32_t popcount = static_cast<uint32_t>(__builtin_popcount(n));
+
+    uint32_t min = ~(~0u << popcount);
+    uint32_t max = min << (32 - popcount);
+    return { max, min };
+}
--- src/Task4.test.cpp
+++ src/Task4.test.cpp
@@ -1,2 +1,14 @@
 #include "../libs/catch.hpp"
+#include "../libs/json.hpp"
 #include "Task4.cpp"
+#include <vector>
+using namespace nlohmann;
+
+/**
+ * Unit tests.
+ */
+
+TEST_CASE("Task 4 test", "[squeezeOutEverythingOfThisBits]") {
+    auto result = squeezeOutEverythingOfThisBits(0b10101);
+    REQUIRE(((json)result).dump() == "[3758096384,7]");
+}
